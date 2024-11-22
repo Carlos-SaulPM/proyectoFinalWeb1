7a1af8c9ff5f189168510f99e074f47a270c26cd
Carlos-SaulPM <carlossaulpazmaldonado@gmail.com>
2024-11-22 13:16:05 -0600

    Arreglando detalles en el index

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Crear la conexión utilizando mysql2
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'user1',
-  password: '198013',  // Asegúrate de usar tu contraseña real aquí
-  database: 'pagina_juegos'
+  host: "localhost",
+  user: "user1",
+  password: "198013", // Asegúrate de usar tu contraseña real aquí
+  database: "pagina_juegos",
 });
 
 // Exportar la conexión para que pueda ser utilizada en otras partes del proyecto
